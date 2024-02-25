e9a62c5ee9365c723dd54bf171156568f6af9090
Edgar Nadal <edgar@two.care>
2024-02-25 13:55:07 -0400

    Update FilterServiceProvider.php

--- src/FilterServiceProvider.php
+++ src/FilterServiceProvider.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace PosLifestyle\DateRangeFilter;
+namespace Craftware\DateRangeFilter;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
